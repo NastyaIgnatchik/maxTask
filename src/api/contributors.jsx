2f817db7d42a76310a7839458a63dbf2cb4c3db5
Nastya <Nastya.igntchk@gmail.com>
2024-03-23 13:33:11 +0300

    init

---
+++ src/api/contributors.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const userInstance = axios.create({
+  headers: {},
+});
+export const githubServices = {
+  async getContributors() {
+    try {
+      return await userInstance.get(
+        "https://api.github.com/repos/koreyspace/generative-ai-for-beginners/contributors"
+      );
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async getUsers(login) {
+    try {
+      return userInstance.get(`https://api.github.com/users/${login}`, {
+        headers: { Authorization: "Bearer " + process.env.REACT_APP_TOKEN },
+      });
+    } catch (error) {
+      return error.response;
+    }
+  },
+};
+
+
+
