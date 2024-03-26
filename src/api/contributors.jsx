480af51c147e32d78a4c3e9000cf8d2b5094886d
Nastya <Nastya.igntchk@gmail.com>
2024-03-26 12:12:37 +0300

    fix

--- src/api/contributors.jsx
+++ src/api/contributors.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const token = "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"
+
 const userInstance = axios.create({
   headers: {},
 });
@@ -17,7 +19,7 @@ export const githubServices = {
   async getUsers(login) {
     try {
       return userInstance.get(`https://api.github.com/users/${login}`, {
-        headers: { Authorization: "Bearer " + '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' },
+        headers: { "Authorization": "Bearer " + `${token}` },
       });
     } catch (error) {
       console.log(error);
